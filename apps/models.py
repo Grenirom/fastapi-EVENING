c866b6ec98495d3e35e303b9d853a6f2805ceb01
Никита Гребнев <nikitagrebnev@MacBook-Air-Nikita.local>
2024-12-04 22:30:41 +0600

    fastapi

---
+++ apps/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, Text
+
+from db_settings import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    quantity = Column(Integer, default=0)
+    description = Column(Text)
---
+++ db_settings.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from decouple import config
+
+db_url = f'postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}/{config('DB_NAME')}'
+
+engine = create_engine(db_url)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
+
